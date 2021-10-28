c5c7ac3c1e8d43623256615dc986428a5f81d0ea
Kevin Salina <kevin.salina25@gmail.com>
2021-10-27 20:44:25 -0400

    Added console.log(err.response) to upload CSV for debugging. Also added reion to AWS config

--- client/src/components/UploadCSVForm/UploadCSVForm.jsx
+++ client/src/components/UploadCSVForm/UploadCSVForm.jsx
@@ -52,7 +52,7 @@ const UploadCSVForm = () => {
       })
       .catch(err => {
         console.log('CSV File upload error frontend', err.message)
-        console.error(err)
+        console.log(err.response)
         setOpen(true)
         setErrorMsg('Cannot upload File.')
       })
--- utilities/upload-aws.js
+++ utilities/upload-aws.js
@@ -6,6 +6,7 @@ const multerS3 = require("multer-s3");
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: 'us-east-1'
 })
 
 const s3 = new AWS.S3()
