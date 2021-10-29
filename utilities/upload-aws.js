1ecac6a890620a8bd99d4db42cf11ac7f73788aa
Kevin Salina <kevin.salina25@gmail.com>
2021-10-29 11:23:29 -0400

    Added support for csv files wiht mime type of vnd.ms-excel

--- utilities/upload-aws.js
+++ utilities/upload-aws.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const AWS = require("aws-sdk");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
+const path = require('path')
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -13,10 +14,11 @@ const s3 = new AWS.S3()
 
 // Multer Only allowing csv files
 const csvFilter = (req, file, cb) => {
-  if (file.mimetype.includes('csv')) {
+  const mimetypes = ['csv', 'vnd.ms-excel']
+  if ((mimetypes.some(mimetype => file.mimetype.includes(mimetype))) && path.extname(file.originalname).toLowerCase() === '.csv') {
     cb(null, true);
   } else {
-    cb('Please upload only CSV file.', false)
+    cb(`Please upload only CSV file. You're file type is ${file.mimetype}`, false)
   }
 };
 
@@ -26,9 +28,9 @@ const upload = multer({
     acl: "public-read",
     s3,
     bucket: process.env.AWS_BUCKET,
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: 'TESTING_METADATA' })
-    },
+    // metadata: function (req, file, cb) {
+    //   cb(null, { fieldName: 'TESTING_METADATA' })
+    // },
     key: function (req, file, cb) {
       cb(null, Date.now().toString())
     }
